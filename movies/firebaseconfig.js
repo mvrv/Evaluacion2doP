ba56d585f9b526f9f4eeff219f07989cf621a390
mvrv <abigaelpl.ti22@utsjr.edu.mx>
2024-10-28 22:16:18 -0600

    tqrqas

--- movies/firebaseconfig.js
+++ movies/firebaseconfig.js
@@ -1,21 +1,25 @@
 const admin = require('firebase-admin');
+require('dotenv').config(); // Asegúrate de que dotenv esté instalado
 
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza correctamente los saltos de línea
+  private_key_id: "6f0024b6c18425c1c499d94e06cfb14747e36a15", // Este debe estar en tu archivo keys.js si decides usar el archivo
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza las secuencias \n por saltos de línea
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
+  client_id: "116057195658126338851",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zip84%40edos-84ac7.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
 };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL, // Incluye esta línea si usas Firebase Realtime Database
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
-module.exports = admin;
+const db = admin.firestore();
+
+module.exports = db;
