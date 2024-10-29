61bbac92e112e5b6c921fc853a1c2a2dbc1e8d2a
mvrv <abigaelpl.ti22@utsjr.edu.mx>
2024-10-28 21:46:54 -0600

    asd

--- movies/firebaseconfig.js
+++ movies/firebaseconfig.js
@@ -4,8 +4,8 @@ require('dotenv').config(); // Asegúrate de que dotenv esté instalado
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: "6f0024b6c18425c1c499d94e06cfb14747e36a15", // Este debe estar en tu archivo keys.js si decides usar el archivo
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza las secuencias \n por saltos de línea
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key_id: "6f0024b6c18425c1c499d94e06cfb14747e36a15",
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: "116057195658126338851",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
